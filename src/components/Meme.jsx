760d71c92af0377f3c828c5bb81c5eb3b4d2dfae
adityadewhy <130918775+adityadewhy@users.noreply.github.com>
2023-10-09 17:33:00 +0000

    formatted form

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 export default function Meme() {
   return (
-    <form>
-      <input type='text' placeholder='top text' className='form--input' />
-      <input type='text' placeholder='bottom text' className='form--input' />
-      <button className='form-btn' > get random meme
-      </button>
-    
-    </form>
+    <>
+
+      <div className='form'>
+        <form>
+          <input type='text' placeholder='top text' className='form--input' />
+          <input type='text' placeholder='bottom text' className='form--input' />
+          <button className='form-btn' > get random meme
+          </button>
+        </form>
+      </div>
+      
+    </>
   )
-  
+
 }
