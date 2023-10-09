a2eab12655e8138c90da8db14691e9aaa58879ab
adityadewhy <130918775+adityadewhy@users.noreply.github.com>
2023-10-09 19:09:50 +0000

    find rendering bug

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,8 +4,8 @@ import memesData from '../memesData.js'
 export default function Meme() {
 
   const [meme, setMeme] = React.useState({
-    topText: "", 
-    bottomText: "", 
+    topText: "",
+    bottomText: "",
     image: "http://i.imgflip.com/1bij.jpg"
   })
 
@@ -17,25 +17,70 @@ export default function Meme() {
     const randomNumber = Math.floor(Math.random() * memesArray.length)
     const url = memesArray[randomNumber].url
     setMeme(prevMeme => ({
-      ...prevMeme, 
+      ...prevMeme,
       image: url
-    }) )
+    }))
   }
 
-    return (
-      <>
+  return (
+    <>
 
-        <div className='form'>
-          <form>
-            <input type='text' placeholder='top text' className='form--input' />
-            <input type='text' placeholder='bottom text' className='form--input' />
-            <button className='form-btn' onClick={getMemeImage}> get random meme
-            </button>
-          </form>
-        </div>
+      <div className='form'>
+        <form>
+          <input type='text' placeholder='top text' className='form--input' />
+          <input type='text' placeholder='bottom text' className='form--input' />
+          <button className='form-btn' onClick={getMemeImage}> get random meme
+          </button>
+        </form>
+      </div>
 
-        <img src={meme.image} className="meme--image" />
+      <img src={meme.image} className="meme--image" />
 
-      </>
-    )
-  }
+    </>
+  )
+}
+// export default function Meme() {
+
+//     const [meme, setMeme] = React.useState({
+//         topText: "",
+//         bottomText: "",
+//         image: "http://i.imgflip.com/1bij.jpg" 
+//     })
+//     const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
+    
+//     function getMemeImage() {
+//         const memesArray = allMemeImages.data.memes
+//         const randomNumber = Math.floor(Math.random() * memesArray.length)
+//         const url = memesArray[randomNumber].url
+//         setMeme(prevMeme => ({
+//             ...prevMeme,
+//             image: url
+//         }))
+        
+//     }
+    
+//     return (
+//         <main>
+//             <div className="form">
+//                 <input 
+//                     type="text"
+//                     placeholder="Top text"
+//                     className="form--input"
+//                 />
+//                 <input 
+//                     type="text"
+//                     placeholder="Bottom text"
+//                     className="form--input"
+//                 />
+//                 <button 
+//                     className="form--button"
+//                     onClick={getMemeImage}
+//                 >
+//                     Get a new meme image 🖼
+//                 </button>
+//             </div>
+//             <img src={meme.image} className="meme--image" />
+//         </main>
+//     )
+// }
