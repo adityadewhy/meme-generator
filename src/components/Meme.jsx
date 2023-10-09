591f0708772d53a5040cc442a80a0f8990343d51
adityadewhy <130918775+adityadewhy@users.noreply.github.com>
2023-10-09 18:27:36 +0000

    try to fix this image state(hook)/render problem

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
+import memesData from '../memesData.js'
 
 export default function Meme() {
-  return (
-    <>
 
-      <div className='form'>
-        <form>
-          <input type='text' placeholder='top text' className='form--input' />
-          <input type='text' placeholder='bottom text' className='form--input' />
-          <button className='form-btn' > get random meme
-          </button>
-        </form>
-      </div>
-      
-    </>
-  )
+  const [meme, setMeme] = React.useState({
+    topText: "", 
+    bottomText: "", 
+    image: "http://i.imgflip.com/1bij.jpg"
+  })
 
-}
+  const [allImages, setAllImages] = React.useState(memesData)
+
+
+  function getMemeImage() {
+    const memesArray = allImages.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+    setMeme(prevMeme => ({
+      ...prevMeme, 
+      image: url
+    }) )
+  }
+
+    return (
+      <>
+
+        <div className='form'>
+          <form>
+            <input type='text' placeholder='top text' className='form--input' />
+            <input type='text' placeholder='bottom text' className='form--input' />
+            <button className='form-btn' onClick={getMemeImage}> get random meme
+            </button>
+          </form>
+        </div>
+
+        <img src={meme.image} className="meme--image" />
+
+      </>
+    )
+  }
