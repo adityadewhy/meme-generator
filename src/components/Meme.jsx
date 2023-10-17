131d8c9568c8f9147af06761065f82144df460c7
adityadewhy <130918775+adityadewhy@users.noreply.github.com>
2023-10-17 17:23:31 +0000

    fix css and project is mostly done

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -11,7 +11,6 @@ export default function Meme() {
 
   const [allImages, setAllImages] = React.useState(memesData)
 
-
   function getMemeImage() {
     const memesArray = allImages.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
@@ -22,16 +21,40 @@ export default function Meme() {
     }))
   }
 
+  function handleChange(event){
+    const {name, value} = event.target
+    setMeme(prevMeme => {
+      return {
+        ...prevMeme,
+        [name]: value
+      }})
+  }
+
   return (
     <>
 
       <div className='form'>
-          <input type='text' placeholder='top text' className='form--input' />
-          <input type='text' placeholder='bottom text' className='form--input' />
+          <input 
+            type='text' 
+            placeholder='top text' 
+            className='form--input'
+            name='topText'
+            value={meme.topText}
+            onChange={handleChange}
+            />
+        
+          <input 
+            type='text' 
+            placeholder='bottom text' 
+            className='form--input' 
+            name= 'bottomText'
+            value= {meme.bottomText}
+            onChange= {handleChange}
+            />
+        
           <button className='form-btn' onClick={getMemeImage}> get random meme
           </button>
       </div>
-
 
       <img src={meme.memeImage} className="meme--image" />
 
